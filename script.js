2b7c6748960d4fc9b74dae2b076a7eada0766fd7
multimediaa <127069296+multimediapwsr@users.noreply.github.com>
2026-02-02 20:58:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,3 @@
-const data = JSON.parse(localStorage.getItem('wartaData'));
-const arsip = JSON.parse(localStorage.getItem('arsipWarta')) || [];
-const ul = document.getElementById('arsipList');
-const selectBulan = document.getElementById('filterBulan');
-const arsipList = document.getElementById('arsipList');
-
-function initArsip(){
-  const bulanSet = new Set(arsip.map(a=>a.bulan));
-  bulanSet.forEach(b=>{
-    const opt = document.createElement('option');
-    opt.value = b;
-    opt.text = formatBulan(b);
-    selectBulan.appendChild(opt);
-  });
-}
-
-function renderArsipFilter(){
-  const bulan = selectBulan.value;
-  arsipList.innerHTML = '';
-
-  arsip
-    .filter(a=>!bulan || a.bulan === bulan)
-    .forEach(a=>{
-      arsipList.innerHTML += `
-        <li>
-          <span>${a.judul}</span>
-          <a href="${a.link}" target="_blank">Download</a>
-        </li>
-      `;
-    });
-}
-
-function formatBulan(b){
-  const [y,m] = b.split('-');
-  const namaBulan = [
-    'Januari','Februari','Maret','April','Mei','Juni',
-    'Juli','Agustus','September','Oktober','November','Desember'
-  ];
-  return `${namaBulan[parseInt(m)-1]} ${y}`;
-}
-
-initArsip();
-renderArsipFilter();
-
-
-arsip.forEach(a=>{
-  ul.innerHTML += `
-    <li>
-      <a href="${a.link}" target="_blank">${a.judul}</a>
-    </li>
-  `;
-});
-
-if(data){
-  document.querySelector('h1').innerText = data.namaGereja;
-  document.getElementById('tglGerejawi').innerText = data.tglGerejawi;
-  document.getElementById('isiWarta').innerHTML = data.isiWarta;
-}
 
 function toggleSidebar(){
   const sb = document.getElementById('sidebar');
@@ -78,14 +20,6 @@ function openSidebar(){
 function closeSidebar(){
   document.getElementById('sidebar').style.left = '-220px';
   document.getElementById('overlay').style.display = 'none';
-}
-
-function downloadWarta(){
-  if(data?.linkWarta){
-    window.open(data.linkWarta,'_blank');
-  }else{
-    alert('Link warta belum diatur admin');
-  }
 }
 
 
